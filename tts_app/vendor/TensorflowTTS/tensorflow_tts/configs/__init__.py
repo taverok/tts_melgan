55e80e3d9faadfc475f89ca9935d68215f860a00
Denis Tokarev <tr.denis@gmail.com>
2020-06-25 00:58:59 +0300

    fixes

---
+++ tts_app/vendor/TensorflowTTS/tensorflow_tts/configs/__init__.py
@@ -0,0 +1,11 @@
+from tensorflow_tts.configs.fastspeech import FastSpeechConfig
+
+from tensorflow_tts.configs.fastspeech2 import FastSpeech2Config
+
+from tensorflow_tts.configs.melgan import MelGANGeneratorConfig
+from tensorflow_tts.configs.melgan import MelGANDiscriminatorConfig
+
+from tensorflow_tts.configs.tacotron2 import Tacotron2Config
+
+from tensorflow_tts.configs.mb_melgan import MultiBandMelGANGeneratorConfig
+from tensorflow_tts.configs.mb_melgan import MultiBandMelGANDiscriminatorConfig
---
+++ tts_app/vendor/TensorflowTTS/tensorflow_tts/models/__init__.py
@@ -0,0 +1,11 @@
+from tensorflow_tts.models.melgan import TFMelGANGenerator
+from tensorflow_tts.models.melgan import TFMelGANDiscriminator
+from tensorflow_tts.models.melgan import TFMelGANMultiScaleDiscriminator
+
+from tensorflow_tts.models.fastspeech import TFFastSpeech
+
+from tensorflow_tts.models.fastspeech2 import TFFastSpeech2
+
+from tensorflow_tts.models.tacotron2 import TFTacotron2
+
+from tensorflow_tts.models.mb_melgan import TFPQMF
